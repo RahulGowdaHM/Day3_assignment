1615d12c824a4eb482670bd93cdf7946a0e3813e
Rahul Gowda <hmrahulgowdru@gmail.com>
2020-07-08 18:35:35 +0530

    Add project files.

---
+++ Day3_Assignment/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Day3_Assignment.Models;
+using Day3_Assignment.Models.Product;
+
+namespace Day3_Assignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<Product> list=new List<Product>()
+        {
+            new Product(){pid="1",pname="Chocolate",stock=50,price=10}
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                list.Add(p);
+                return RedirectToAction("GetAll");
+            }
+            else
+            {
+                return View("Create");
+            }
+        }
+        public IActionResult GetAll(String uid)
+        {
+            Product p = list.SingleOrDefault(i => i.pid == uid);
+            return View(p);
+        }
+    }
+}
---
+++ Day3_Assignment/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Day3_Assignment.Models.Product
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Product id required")]
+        public string pid { get; set; }
+        [Required(ErrorMessage = "Product name required")]
+        public string pname { get; set; }
+        [Required(ErrorMessage = "Product price required")]
+        public int price { get; set; }
+        [Required(ErrorMessage = "Stock required")]
+        public int stock { get; set; }
+    }
+}
